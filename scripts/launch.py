eb3af97d9b8d0079eb36488bd5ef224288c7f3e4
Nicolas KOWALSKI <nicolas.kowalski@usherbrooke.ca>
2020-11-15 13:50:54 -0800

    Changed the command to launch jobs and accept them more easily

--- scripts/launch.py
+++ scripts/launch.py
@@ -17,6 +17,12 @@ def run(iterations_max,files_dir,iteration_start = 0):
 	#files_dir is here the path of the folder to launch from the main dir (example ComputedData/ep9.0_beta60.0_mu12.41_U12.0_tpd1.4_tppp1.0)
 	path_to_main_dir = "../"
 	os.chdir(os.path.join(os.path.dirname(os.path.realpath(__file__)))) #The working directory is now the scripts directory
+
+	host_file = os.path.join(files_dir,"hosts")
+	f = open(host_file,"w")
+	call(["slurm_hl2hl.py","--format","MPIHOSTLIST"],stdout=f)
+	f.close()
+
 	iterations=iteration_start
 	autocoherence_dir = os.path.join(path_to_main_dir,"Autocoherence")
 	solver_dir = os.path.join(path_to_main_dir,"ImpuritySolver")
@@ -24,7 +30,11 @@ def run(iterations_max,files_dir,iteration_start = 0):
 	output_dir = os.path.join(os.path.join(files_dir,"OUT"),"")
 
 	data_dir = os.path.join(os.path.join(files_dir,"DATA"),"")
-	to_log("Let's go for " + str(iterations_max) + " iterations" + "\n")
+	if iterations_max != -1:
+		to_log("Let's go for " + str(iterations_max) + " iterations" + "\n")
+	else:
+		to_log("Let's go for some iterations" + "\n")
+
 	if iterations == 0:
 		#In case you need to start from scratch (not working for now)
 		call([os.path.join(autocoherence_dir,"CDMFT"), output_dir, input_dir, data_dir, "params", str(iterations)])
@@ -36,7 +46,7 @@ def run(iterations_max,files_dir,iteration_start = 0):
 			to_log("There was an error, an input parameters file was not produced ("+ input_dir + "params" + str(iteration_number) + ".json" + "), Retrying the iteration before. Try number " + str(number_of_tries))
 			return -1
 		to_log("Calling the Impurity Solver")
-		call(["srun", os.path.join(solver_dir,"IS"),input_dir, output_dir,"params" + str(iterations)])
+		call(["mpiexec", "--hostfile", host_file, os.path.join(solver_dir,"IS"),input_dir, output_dir,"params" + str(iterations)])
 		to_log("Calling the Autocoherence")
 		call([os.path.join(autocoherence_dir,"CDMFT"), output_dir, input_dir, data_dir, "params", str(iterations)])
 		to_log("end iteration " + str(iterations)  + " at: " + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")) + "\n")
@@ -47,7 +57,7 @@ def run(iterations_max,files_dir,iteration_start = 0):
 		call(["./actions.py", "-a","order_parameter", "-f",os.path.basename(files_dir)])
 		return 0
 	number_of_tries = 0
-	while iterations <= iterations_max and number_of_tries <= max_number_of_tries:
+	while (iterations_max == -1 or iterations <= iterations_max) and number_of_tries <= max_number_of_tries:
 		if do_one_iteration(iterations) == 0:
 			iterations+=1
 		else: #The only case programmed is when input files have not been produced for the current iteration, it means something happened during the last iteration, we therefore have to retry the last one 
