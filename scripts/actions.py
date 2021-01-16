f847419e45f766038c7bf43ea24d3653bcc6edba
Nicolas KOWALSKI <nicolas.kowalski@usherbrooke.ca>
2021-01-16 08:45:52 -0800

    Simplified the calculation of the order_parameter

--- scripts/actions.py
+++ scripts/actions.py
@@ -14,114 +14,116 @@ multiple_filenames = ["ChiFull","ChiFullSites","dgreen","green","hyb","pK","pxgr
 
 #Parser that prints the help on error
 class MyParser(argparse.ArgumentParser): 
-   def error(self, message):
-      sys.stderr.write('error: %s\n' % message)
-      self.print_help()
-      sys.exit(2)
+    def error(self, message):
+        sys.stderr.write('error: %s\n' % message)
+        self.print_help()
+        sys.exit(2)
 #END
 
 #Beginning function to handle uncomputed occupations
 def find_missing_occupation(input_dir,data_dir):
-	try:
-		#We want to get all the iterations for which the occupation was already computed
-		lst = np.sort(np.loadtxt(os.path.join(data_dir,"ekin.dat"))[:,0].astype(int))
-	except Exception as e:
-		#If that doesn't work, it means either we have no pn file of too few data (only one line)
-		print("First problem : " + str(e))
-		try:
-			#Trying to see if there is at least a line in the file
-			lst = [1,np.loadtxt(os.path.join(data_dir,"ekin.dat"))[0]]
-		except Exception as e:
-			#If not then just do the first iteration
-			print("Second problem : " + str(e))
-			return 1
-	should_be = np.sort(np.loadtxt(os.path.join(data_dir,"N.dat"))[:,0].astype(int))
-	for i in should_be:
-		if i not in lst:
-			return i
-	return None
+    try:
+        #We want to get all the iterations for which the occupation was already computed
+        lst = np.sort(np.loadtxt(os.path.join(data_dir,"ekin.dat"))[:,0].astype(int))
+    except Exception as e:
+        #If that doesn't work, it means either we have no pn file of too few data (only one line)
+        print("First problem : " + str(e))
+        try:
+            #Trying to see if there is at least a line in the file
+            lst = [1,np.loadtxt(os.path.join(data_dir,"ekin.dat"))[0]]
+        except Exception as e:
+            #If not then just do the first iteration
+            print("Second problem : " + str(e))
+            return 1
+    should_be = np.sort(np.loadtxt(os.path.join(data_dir,"N.dat"))[:,0].astype(int))
+    for i in should_be:
+        if i not in lst:
+            return i
+    return None
+
+
 def all_occupations_in_folder(files_dir):
-	data_from_autocoherence = "../"
-	autocoherence_dir = "Autocoherence/"
-	solver_dir = "ImpuritySolver/"
-	input_dir = os.path.join(files_dir,"IN/")
-	output_dir = os.path.join(files_dir,"OUT/")
-	data_dir = os.path.join(files_dir,"DATA/")
-	all_data_dir = os.path.join(files_dir,"../")
-	index = find_missing_occupation(input_dir,data_dir)
-	if index != None:
-		os.chdir("scripts")
-		run(["./run_occupation.sh", os.path.join(data_from_autocoherence,output_dir), os.path.join(data_from_autocoherence,data_dir), "params", str(index)])
-		run(["sbatch","actions.sh","-a","occupations","-f",os.path.basename(os.path.normpath(files_dir))])
-	else:
-		for file in single_occupation_files:
-			data = np.loadtxt(os.path.join(data_dir,file))
-			data = data[np.argsort(data[:,0])]
-			np.savetxt(os.path.join(data_dir,file),data,fmt='%i %f')
-			order_and_remove_occupation(files_dir)
-		#We need to sort everything to be sure it's ok
-	return 0
+    data_from_autocoherence = "../"
+    autocoherence_dir = "Autocoherence/"
+    solver_dir = "ImpuritySolver/"
+    input_dir = os.path.join(files_dir,"IN/")
+    output_dir = os.path.join(files_dir,"OUT/")
+    data_dir = os.path.join(files_dir,"DATA/")
+    all_data_dir = os.path.join(files_dir,"../")
+    index = find_missing_occupation(input_dir,data_dir)
+    if index != None:
+        os.chdir("scripts")
+        run(["./run_occupation.sh", os.path.join(data_from_autocoherence,output_dir), os.path.join(data_from_autocoherence,data_dir), "params", str(index)])
+        run(["sbatch","actions.sh","-a","occupations","-f",os.path.basename(os.path.normpath(files_dir))])
+    else:
+        for file in single_occupation_files:
+            data = np.loadtxt(os.path.join(data_dir,file))
+            data = data[np.argsort(data[:,0])]
+            np.savetxt(os.path.join(data_dir,file),data,fmt='%i %f')
+            order_and_remove_occupation(files_dir)
+            #We need to sort everything to be sure it's ok
+    return 0
 def order_and_remove_occupation(files_dir):
-	autocoherence_dir = "Autocoherence/"
-	solver_dir = "ImpuritySolver/"
-	input_dir = os.path.join(files_dir,"IN/")
-	output_dir = os.path.join(files_dir,"OUT/")
-	data_dir = os.path.join(files_dir,"DATA/")
-	all_data_dir = os.path.join(files_dir,"../")
-	try:
-		for file in single_filenames:
-			data = np.loadtxt(os.path.join(data_dir,file))
-			treated = data[np.unique(data[:,0],return_index = True)[1],:]
-			try:
-				np.savetxt(os.path.join(data_dir,file),treated,fmt="%i %f")
-			except:
-				np.savetxt(os.path.join(data_dir,file),treated,fmt="%i %f %f %f %f")
-	except:
-		pass
-	return 0
+    autocoherence_dir = "Autocoherence/"
+    solver_dir = "ImpuritySolver/"
+    input_dir = os.path.join(files_dir,"IN/")
+    output_dir = os.path.join(files_dir,"OUT/")
+    data_dir = os.path.join(files_dir,"DATA/")
+    all_data_dir = os.path.join(files_dir,"../")
+    try:
+        for file in single_filenames:
+            data = np.loadtxt(os.path.join(data_dir,file))
+            treated = data[np.unique(data[:,0],return_index = True)[1],:]
+            try:
+                np.savetxt(os.path.join(data_dir,file),treated,fmt="%i %f")
+            except:
+                np.savetxt(os.path.join(data_dir,file),treated,fmt="%i %f %f %f %f")
+    except:
+        pass
+    return 0
 def handle_occupation(files_dir):
-	order_and_remove_occupation(files_dir)
-	return all_occupations_in_folder(files_dir)
+    order_and_remove_occupation(files_dir)
+    return all_occupations_in_folder(files_dir)
 #END
 
 #Beginnning functions to delete the last iteration
 def find_last_results(files_dir):
-	results_list = []
-	for f in glob.glob(os.path.join(files_dir,"OUT/params*.meas.json")):
-		results_list.append(int(re.search(r"params([0-9]+)\.meas\.json",f).group(1)))
-	try:
-		return(max(results_list))
-	except:
-		print("There is no output to delete, take a look")
-		exit()
+    results_list = []
+    for f in glob.glob(os.path.join(files_dir,"OUT/params*.meas.json")):
+        results_list.append(int(re.search(r"params([0-9]+)\.meas\.json",f).group(1)))
+    try:
+        return(max(results_list))
+    except:
+        print("There is no output to delete, take a look")
+        exit()
 def delete_safely(file):
-	try:
-		os.remove(file)
-	except Exception as e:
-		print("Erreur au fichier " + file)
+    try:
+        os.remove(file)
+    except Exception as e:
+        print("Erreur au fichier " + file)
 def delete_last(files_dir):
-	iteration = find_last_results(files_dir)
-	data_dir = os.path.join(files_dir,"DATA/")
-	input_dir = os.path.join(files_dir,"IN/")
-	output_dir = os.path.join(files_dir,"OUT/")
-	for file in single_filenames:
-		try:
-			data = np.loadtxt(os.path.join(data_dir,file))
-			if int(data[-1,0]) == int(iteration):
-				try:
-					np.savetxt(os.path.join(data_dir,file),data[:-1,:],fmt="%i %f")
-				except:
-						np.savetxt(os.path.join(data_dir,file),data[:-1,:],fmt="%i %f %f %f %f")
-			else:
-				print("Erreur au fichier " + file + " " + str(int(data[-1,0])) + " :  " + str(int(iteration)))
-		except Exception as e:
-			print("Erreur au fichier " + file)
-	for file in multiple_filenames:
-		delete_safely(os.path.join(data_dir,file + str(iteration) + ".dat"))
-	delete_safely(os.path.join(input_dir,"Hyb" + str(int(iteration)+1) + ".json"))	
-	delete_safely(os.path.join(input_dir,"params" + str(int(iteration)+1) + ".json"))		
-	delete_safely(os.path.join(output_dir,"params" + str(iteration) + ".meas.json"))	
-	return 0
+    iteration = find_last_results(files_dir)
+    data_dir = os.path.join(files_dir,"DATA/")
+    input_dir = os.path.join(files_dir,"IN/")
+    output_dir = os.path.join(files_dir,"OUT/")
+    for file in single_filenames:
+        try:
+            data = np.loadtxt(os.path.join(data_dir,file))
+            if int(data[-1,0]) == int(iteration):
+                try:
+                    np.savetxt(os.path.join(data_dir,file),data[:-1,:],fmt="%i %f")
+                except:
+                        np.savetxt(os.path.join(data_dir,file),data[:-1,:],fmt="%i %f %f %f %f")
+            else:
+                print("Erreur au fichier " + file + " " + str(int(data[-1,0])) + " :  " + str(int(iteration)))
+        except Exception as e:
+            print("Erreur au fichier " + file)
+    for file in multiple_filenames:
+        delete_safely(os.path.join(data_dir,file + str(iteration) + ".dat"))
+    delete_safely(os.path.join(input_dir,"Hyb" + str(int(iteration)+1) + ".json"))  
+    delete_safely(os.path.join(input_dir,"params" + str(int(iteration)+1) + ".json"))       
+    delete_safely(os.path.join(output_dir,"params" + str(iteration) + ".meas.json"))    
+    return 0
 #END
 
 #Beginning functions to prepare the folder for copy to the local computer
@@ -144,7 +146,7 @@ def delete_useless(folder_name):
         os.remove(f)
 
 def prepare_copy(folder_name):
-	delete_useless(folder_name)
+    delete_useless(folder_name)
 #END
 
 #Beginning functions to compute the order parameter for all iterations that were not already computed
@@ -155,37 +157,33 @@ def compute_order_parameter(folder_name):
     graph = np.zeros(0)
     G = []
     offset = 1
+    #If there is one, load the existing order_parameter file
     try:
         graph = np.load(save_filename + ".npy")
         offset+=len(graph)
     except:
         pass
+    #Now we compute the order parameter for the iterations in which it has not been done yet
     i=offset
     try:
         while(True):
-            G.append([])
-            f=open(folder_name + "/DATA/" + filename + str(i) + ".dat", "r")
-            fl = f.readlines()
-            for num, line in enumerate(fl):
-                valueList = re.split(" |\n",line)[1:-1]
-                G[i-offset].append(valueList)
+            G_data = np.loadtxt(os.path.join(folder_name,"DATA/" + filename + str(i) + ".dat"))
+            G.append((G_data[:,1+2*(Composante+1)] - G_data[:,1+2*Composante])/2)
             i+=1
-            f.close()
-    except FileNotFoundError as e:
+
+    except Exception as e:
         print("Fin de la lecture des fichiers, le dernier était le numéro: " + str(i-1))
         if i-1 == 0:
             print(e)
-    except Exception as e:
-        print(str(e))
-    if len(G[0]) != 0:
-        G = np.array(G[:-1]).astype(float)
-        print(G.shape)
-        #Ici, G[it][iwn][site]
-        G = np.transpose(G,(1,2,0)) #Ici, G[iwn][site][it], mieux pour voir quelquechose
-        with open(folder_name + "/IN/params1.json") as f:
+    #If we loaded some Green's function components, we compute the order parameter and add it to the existing computations
+    if G:     
+        #Here we have G = order_parameter[it][iwn]
+        #We load beta
+        with open(folder_name + "/IN/params" + str(i-1) + ".json") as f:
             beta = json.load(f)["beta"]
-        G = np.sum(G,axis=0)*2/beta
-        graph = np.concatenate((graph,(G[2*(Composante+1)] - G[2*Composante])/2))
+        #We sum over the Matsubara frequencies
+        G = np.sum(G,axis=1)*2/beta
+        graph = np.concatenate((graph,G))
         np.save(save_filename,graph)
 #END
 
@@ -202,7 +200,7 @@ parser.add_argument("-a",dest="action",help="the action of the program among : \
 parser.add_argument("-f",dest="folder",help="the folder you want to act on",required=True)
 args = parser.parse_args()
 if args.action not in actions_list:
-	parser.error("-a should be in " + str(actions_list))
+    parser.error("-a should be in " + str(actions_list))
 
 #Get the function we want to launch
 index_in_list = actions_list.index(args.action)
